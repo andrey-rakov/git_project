8366d88c88d609ba25d0a52df6abe7564399bc3b
Andery Rakov <rakov80@mail.ru>
2019-10-03 00:56:56 +0700

    Добавлен рисунок флагштока

--- project_pygame_0.py
+++ project_pygame_0.py
@@ -6,7 +6,7 @@ pygame.display.set_caption('Рисуем флаг')
 size = 300, 300
 screen = pygame.display.set_mode(size)
 
-pygame.draw.rect(screen, pygame.Color('red'), (0, 0, 100, 100), 0)
+pygame.draw.rect(screen, pygame.Color('brown'), (0, 0, 10, 300), 0)
 
 while pygame.event.wait().type != pygame.QUIT:
     pygame.display.flip()
